b8287a2c90779030f62c73bcc1f0604618db1024
Veesa <veesajackson@gmail.com>
2025-02-20 04:21:53 +0530

    Update frontend dependencies to fix build error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,18 +15,10 @@ function App() {
   return (
     <Provider store={store}>
       <Router>
-        <div className="min-h-screen bg-gray-100">
-          <Navbar />
-          <main className="container mx-auto px-4 py-8">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/book" element={<RideBooking />} />
-              <Route path="/history" element={<RideHistory />} />
-              <Route path="/profile" element={<Profile />} />
-            </Routes>
-          </main>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<RideBooking />} />
+          </Routes>
         </div>
       </Router>
     </Provider>
